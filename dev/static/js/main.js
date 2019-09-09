ed60959f6732b39ddc435e6be7da335e4d64dce3
Amorphis81 <petikantrop@gmail.com>
2019-09-09 23:34:27 +0300

    News + FAQ

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -36,6 +36,16 @@ $(document).ready(function () {
             $(myorderTab[i]).addClass('myorder-tab-row-grey');
         }
     }
+    
+    //News bg
+    let newsBlock = $('.news-block');
+    let newsBlockLength = $('.news-block').length;
+    
+    for (let i = 0; i < newsBlockLength; i++){
+        if (i%2 == 1) {
+            $(newsBlock[i]).addClass('news-block-dark');
+        }
+    }
 
     //Catalog popup
     $('.button.service-block__more').on('click', function() {
@@ -48,6 +58,17 @@ $(document).ready(function () {
 
     $('.popup__close').on('click', function() {
         $(this).parent().removeClass('popup-visible');
+    })
+
+    //FAQ
+    $('.faq-block-question').on('click', function(){
+        // $(this).parent().find('.faq-block-answer').toggleClass('faq-block-answer-dropdown').visible(); 
+        if($(this).parent().find('.faq-block-answer:hidden').length == 1){
+            $(this).parent().find('.faq-block-answer').show('fast');
+        }
+        else if($(this).parent().find('.faq-block-answer:hidden').length == 0){
+            $(this).parent().find('.faq-block-answer').hide('fast');
+        }
     })
 
 });
